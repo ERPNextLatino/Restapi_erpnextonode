17833865473ff5c21b44bcb6008c2ecbd253e383
Fredy Saul teheran Tovar <fteheran@tinformatica.com>
2018-12-10 16:34:12 -0500

    x

--- index.js
+++ index.js
@@ -43,14 +43,15 @@ APIRESTERPNext.prototype.login = function () {
 }
 APIRESTERPNext.prototype.loginUser = function (req) {
     var _this = req;
+    var __this = this
     var formData = querystring.stringify({
         usr: _this.username,
         pwd: _this.password
     });
     var contentLength = formData.length;
     return requestPromise.post({
-        url: _this.baseUrl + "/api/method/login",
-        jar: _this.cookieJar,
+        url: __this.baseUrl + "/api/method/login",
+        jar: __this.cookieJar,
         body: formData,
         headers: {
             'Content-Length': contentLength,
